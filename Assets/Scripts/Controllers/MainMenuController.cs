d796e9500956645d0b5d1842455ff623dcce6211
Dillan Prince <dillan.prince@theagileist.com>
2016-08-25 19:37:59 -0500

    Init

---
+++ Assets/Scripts/Controllers/MainMenuController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Linq;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject _newGameDialog;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ShowNewGameDialog()
+    {
+        _newGameDialog.SetActive(true);
+    }
+
+    public void ConfirmNewGame()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("Game");
+    }
+
+    public void DenyNewGame()
+    {
+        _newGameDialog.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Controllers/SettingsController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Linq;
+
+public class SettingsController : MonoBehaviour
+{
+    public GameObject _settingsDialogPrefab;
+
+    private static int _volume;
+
+    void Awake()
+    {
+        LoadSettings();
+    }
+
+    public void ShowSettingsDialog()
+    {
+        GameObject settingsDialog = Instantiate(_settingsDialogPrefab);
+        Configure(settingsDialog);
+    }
+
+    private void Configure(GameObject settingsDialog)
+    {
+        settingsDialog.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+
+        Text volumeValueText = settingsDialog.GetComponentsInChildren<Text>().FirstOrDefault(t => t.name == "VolumeValueText");
+        volumeValueText.text = _volume.ToString() + "%";
+
+        Slider volumeSlider = settingsDialog.GetComponentsInChildren<Slider>().FirstOrDefault(s => s.name == "VolumeSlider");
+        volumeSlider.onValueChanged.AddListener((value) => { volumeValueText.text = value.ToString() + "%"; });
+        volumeSlider.value = _volume;
+
+        ConfigureButtons(settingsDialog);
+    }
+
+    private void ConfigureButtons(GameObject settingsDialog)
+    {
+        Button exitButton = settingsDialog.GetComponentsInChildren<Button>().FirstOrDefault(b => b.name == "ExitButton");
+        exitButton.onClick.AddListener(() => { Destroy(settingsDialog); });
+
+        Button saveButton = settingsDialog.GetComponentsInChildren<Button>().FirstOrDefault(b => b.name == "SaveButton");
+        saveButton.onClick.AddListener(() => { SaveSettings(settingsDialog); });
+
+        Button cancelButton = settingsDialog.GetComponentsInChildren<Button>().FirstOrDefault(b => b.name == "CancelButton");
+        cancelButton.onClick.AddListener(() =>
+        {
+            settingsDialog.GetComponentsInChildren<Slider>().FirstOrDefault(s => s.name == "VolumeSlider").value = _volume;
+            settingsDialog.GetComponentsInChildren<Text>().FirstOrDefault(t => t.name == "VolumeValueText").text = _volume.ToString() + "%";
+        });
+
+    }
+
+    private void SaveSettings(GameObject settingsDialog)
+    {
+        _volume = (int)settingsDialog.GetComponentsInChildren<Slider>().FirstOrDefault(s => s.name == "VolumeSlider").value;
+        PlayerPrefs.SetInt("volume", _volume);
+    }
+
+    private void LoadSettings()
+    {
+        _volume = PlayerPrefs.GetInt("volume", 100);
+    }
+}
